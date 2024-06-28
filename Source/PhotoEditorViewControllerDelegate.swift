705a7293723fb5d059b0a36a027aded55769ecb2
Igor Bezlepkin <hellobezlepkin@gmail.com>
2024-06-28 12:26:21 +0300

    Moved PhotoEditorDelegate to root

---
+++ Source/PhotoEditorViewControllerDelegate.swift
@@ -0,0 +1,10 @@
+public protocol PhotoEditorDelegate {
+    /**
+     - Parameter image: edited Image
+     */
+    func doneEditing(image: UIImage)
+    /**
+     StickersViewController did Disappear
+     */
+    func canceledEditing()
+}
