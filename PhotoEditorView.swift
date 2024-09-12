e3486912948d444608c334c4f77d8b0938c0240b
Igor Bezlepkin <hellobezlepkin@gmail.com>
2024-09-12 15:15:19 +0300

    -

--- PhotoEditorView.swift
+++ PhotoEditorView.swift
@@ -8,4 +8,109 @@
 import Foundation
 
 public final class PhotoEditorView: UIView {
+    var canvasView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    var imageViewHeightConstraint: NSLayoutConstraint!
+    var canvasImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    var topGradient: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    var topToolbar: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    private var topToolBarStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 15
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    var bottomGradient: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    var bottomToolbar: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    private var bottomToolBarStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 15
+        stackView.alignment = .fill
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    var doneButton: HiglihtedButton = {
+        let button = HiglihtedButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Done", for: .normal)
+        button.isHidden = true
+        button.titleLabel?.font = UIFont(name: "System Semibold ", size: 16)
+        return button
+    }()
+    
+    var deleteView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        view.isHidden = true
+        return view
+    }()
+    var deleteLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isUserInteractionEnabled = false
+        label.font = UIFont(name: "icomoon", size: 30)
+        label.textColor = .white
+        label.text = ""
+        return label
+    }()
+    var colorPickerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isHidden = true
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        return view
+    }()
+    var colorPickerViewBottomConstraint: NSLayoutConstraint!
+    
+    var colorsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        // collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.backgroundColor = UIColor.black
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
 }
