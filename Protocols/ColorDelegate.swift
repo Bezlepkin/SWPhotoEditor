431034b2a20d44097c011297ba1c9a45abe9a7d3
Igor Bezlepkin <hellobezlepkin@gmail.com>
2024-09-25 11:09:05 +0300

    -

---
+++ Protocols/ColorDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  ColorDelegate.swift
+//  PhotoEditor
+//
+//  Created by Igor Bezlepkin on 25.09.2024.
+//
+
+import Foundation
---
+++ Protocols/PhotoEditorDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoEditorDelegate.swift
+//  PhotoEditor
+//
+//  Created by Igor Bezlepkin on 25.09.2024.
+//
+
+import Foundation
